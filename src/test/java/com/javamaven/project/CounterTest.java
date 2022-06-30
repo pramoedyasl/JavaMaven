a8dd81b88537d77689a9551a7e73590316cb0891
biagiusama <79629763+biagiusama@users.noreply.github.com>
2022-06-30 19:52:56 +0700

    Test

---
+++ src/test/java/com/javamaven/project/CounterTest.java
@@ -0,0 +1,28 @@
+package com.javamaven.project;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CounterTest {
+
+	@Test
+    public void testAdd() {
+        assertEquals(34, new Driver().Add(8, 26).getResult());
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(4, new Driver().Sub(12, 8).getResult());
+    }
+
+    @Test
+    public void testMul() {
+        assertEquals(88, new Driver().Mul(11, 8).getResult());
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(1, new Driver().Div(12, 12).getResult());
+    }
+
+}
