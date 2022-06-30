c9a8632670bb8599f5862513570eb2054e5c61d9
pramoedyasl <79629980+pramoedyasl@users.noreply.github.com>
2022-06-30 05:08:06 -0700

    Update Driver.java

--- src/main/java/com/javamaven/project/Driver.java
+++ src/main/java/com/javamaven/project/Driver.java
@@ -4,20 +4,20 @@ import javax.ws.rs.QueryParam;
 
 public class Driver {
 
-	public Counter Add(@QueryParam("x") int x, @QueryParam("y") int y) {
-        return new Counter(x, y, x + y);
+	public Counters Add(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counters(x, y, x + y);
     }
 
-    public Counter Sub(@QueryParam("x") int x, @QueryParam("y") int y) {
-        return new Counter(x, y, x - y);
+    public Counters Sub(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counters(x, y, x - y);
     }
 
-    public Counter Mul(@QueryParam("x") int x, @QueryParam("y") int y) {
-        return new Counter(x, y, x * y);
+    public Counters Mul(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counters(x, y, x * y);
     }
 
-    public Counter Div(@QueryParam("x") int x, @QueryParam("y") int y) {
-        return new Counter(x, y, x / y);
+    public Counters Div(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counters(x, y, x / y);
     }
 
 }
