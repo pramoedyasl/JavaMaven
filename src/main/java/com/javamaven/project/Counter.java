19888564b02669a65fbfe2b10b8598cb7c822556
pramoedyasl <pramoedyasl@student.telkomuniversity.ac.id>
2022-06-30 18:26:35 +0700

    Initial commit
    Continuous Integration

---
+++ src/main/java/com/javamaven/project/Counter.java
@@ -0,0 +1,28 @@
+package com.javamaven.project;
+
+public class Counter {
+
+	int _x;
+    int _y;
+    int _result;
+
+    public Counter(int x, int y, int result) {
+        _x = x;
+        _y = y;
+        _result = result;
+    }
+
+    public int getX() {
+        return _x;
+    }
+
+    public int getY() {
+        return _y;
+    }
+
+    public int getResult() {
+        return _result;
+    }
+
+    
+}
---
+++ src/main/java/com/javamaven/project/Driver.java
@@ -0,0 +1,23 @@
+package com.javamaven.project;
+
+import javax.ws.rs.QueryParam;
+
+public class Driver {
+
+	public Counter Add(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counter(x, y, x + y);
+    }
+
+    public Counter Sub(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counter(x, y, x - y);
+    }
+
+    public Counter Mul(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counter(x, y, x * y);
+    }
+
+    public Counter Div(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new Counter(x, y, x / y);
+    }
+
+}
